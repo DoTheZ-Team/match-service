d44d02a9d6ba1e6acaa285f951840ca6346a15d1
이주애 <joojuae02@gachon.ac.kr>
2024-05-03 14:34:29 +0900

    Revert "feat: api 연결 완료"
    This reverts commit 3163df443bdd45a7e0cf14830dba296f798a9b72.

--- src/main/java/com/justdo/glue/sticker/global/response/code/BaseErrorCode.java
+++
@@ -1,8 +0,0 @@
-package com.justdo.glue.sticker.global.response.code;
-
-public interface BaseErrorCode {
-
-    public ErrorReasonDto getReason();
-
-    public ErrorReasonDto getReasonHttpStatus();
-}
--- src/main/java/com/justdo/glue/sticker/global/utils/JwtProperties.java
+++
@@ -1,9 +0,0 @@
-package com.justdo.glue.sticker.global.utils;
-
-public interface JwtProperties {
-
-    String TOKEN_PREFIX = "Bearer ";
-    String HEADER_AUTHORIZATION = "Authorization";
-    String USER_ID = "userId";
-    Integer TOKEN_SPLIT = 7;
-}
