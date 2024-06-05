2069b36fc437fb2af49b888750f2cc9edf11f717
gyeom <rlagusrua3687@gmail.com>
2024-06-06 03:12:47 +0900

    GLUE-372 fix: PostSticker 조회 로직 수정

--- src/main/java/com/justdo/glue/sticker/domain/poststicker/service/PostStickerQueryService.java
+++ src/main/java/com/justdo/glue/sticker/domain/poststicker/service/PostStickerQueryService.java
@@ -1,11 +1,12 @@
 package com.justdo.glue.sticker.domain.poststicker.service;
 
-import com.justdo.glue.sticker.domain.poststicker.PostSticker;
 import com.justdo.glue.sticker.domain.poststicker.dto.PostStickerDTO;
-import org.springframework.data.domain.Page;
+import com.justdo.glue.sticker.domain.poststicker.dto.PostStickerDTO.PostStickerItem;
+import java.util.List;
 
 public interface PostStickerQueryService {
+
     PostStickerDTO.PostStickerItem getPostStickerById(Long id);
-    PostStickerDTO.PostStickerItem savePostSticker(PostSticker postSticker);
-    PostStickerDTO.PostStickerUrlItems getPostStickersByPostId(Long postId);
+
+    List<PostStickerItem> getPostStickersByPostId(Long postId);
 }
